41cd6b1ff7e6782d1411039611bb1534c951522d
Guduru <PGUDURU1@ford.com>
2019-09-04 16:11:26 -0400

    updated

---
+++ src/MyForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+
+
+function MyForm() {
+
+
+
+  
+    return (
+
+        <div class="col-md pr-md-12 mb-12">
+        <form action="" method="">
+            <div class="form-group">
+                <input type="button" class="btn btn-primary" value="JOIN SUNIL"/>
+              </div>
+          <div class="form-group">
+            <label for="name" class="sr-only">Name</label>
+            <input type="text" class="form-control bg-custom-1" id="name" placeholder="Enter your name"/>
+          </div>
+          <div class="form-group">
+            <label for="email" class="sr-only">Email</label>
+            <input type="text" class="form-control" id="email" placeholder="Enter your email"/>
+          </div>
+         
+         
+        </form>
+      </div>
+  
+  
+      
+  
+      
+    );
+  }
+
+  export default MyForm;
