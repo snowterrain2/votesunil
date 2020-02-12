8593af355978be32fdbb9433373279b2b9b5c452
snowterrain2 <39038695+snowterrain2@users.noreply.github.com>
2020-02-12 13:56:41 -0500

    removed endors

--- src/yjson.js
+++ src/yjson.js
@@ -1,14 +1,7 @@
 export const  youtubeEndorsements = {"1":{"label":"Former Mayor Jeanne Stine","value":"Uwu9Sr3598g"},
-                "2":{"label":"Former city county commissioner Wade Fleming","value":"W14ZoXfutow"},
-                "3":{"label":"Former Troy School principal Gary Wood","value":"4MzSFXQvVaU"},
-                "4":{"label":"Precinct delegate Ray Watts","value":"S_XtE6RE_kM"},
-                "5":{"label":"Dhanya Vani Rao","value":"SeCs396SLWE"},
-                "6":{"label":"Elizabeth","value":"0ecgDwQcTvk"},
-                "7":{"label":"Congressman Dave Trott","value":"VtT_Aqu5A-E"},
-                "8":{"label":"Jim Werpetenski","value":"cP-NTQHBe9E"},
-				"9":{"label":"Former Rep. Rocky Raczkowski","value":"b8I3Jke6yX8"}
-				
-            
-            
-            
+               "2":{"label":"Former Troy School principal Gary Wood","value":"4MzSFXQvVaU"},
+                "3":{"label":"Dhanya Vani Rao","value":"SeCs396SLWE"},
+                "4":{"label":"Elizabeth","value":"0ecgDwQcTvk"},
+                "5":{"label":"Jim Werpetenski","value":"cP-NTQHBe9E"}
+           
             };
